79dc03fbf04dfef463e6192f17128724a245c58d
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-09-23 13:07:57 +0530

    PhaseEndProject

---
+++ P4PhaseEndProject/Models/P4PhaseEndProjectContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace P4PhaseEndProject.Models;
+
+public partial class P4PhaseEndProjectContext : DbContext
+{
+    public P4PhaseEndProjectContext()
+    {
+    }
+
+    public P4PhaseEndProjectContext(DbContextOptions<P4PhaseEndProjectContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<CustLogInfo> CustLogInfos { get; set; }
+
+    public virtual DbSet<UserInfo> UserInfos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=tcp:harsha1324.database.windows.net,1433;Initial Catalog = myloginDb; User Id = harsha; Password=amareshAM@12345 Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CustLogInfo>(entity =>
+        {
+            entity.HasKey(e => e.LogId).HasName("PK__CustLogI__5E548648CD131032");
+
+            entity.ToTable("CustLogInfo");
+
+            entity.Property(e => e.LogId).ValueGeneratedNever();
+            entity.Property(e => e.CustEmail).HasMaxLength(100);
+            entity.Property(e => e.CustName).HasMaxLength(50);
+            entity.Property(e => e.Description).HasMaxLength(50);
+            entity.Property(e => e.LogStatus).HasMaxLength(50);
+
+            entity.HasOne(d => d.User).WithMany(p => p.CustLogInfos)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__CustLogIn__UserI__619B8048");
+        });
+
+        modelBuilder.Entity<UserInfo>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__UserInfo__1788CC4CC1CA7163");
+
+            entity.ToTable("UserInfo");
+
+            entity.Property(e => e.UserId).ValueGeneratedNever();
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ P4PhaseEndProject/Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace P4PhaseEndProject.Models
+{
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public virtual ICollection<CustLogInfo> CustLogInfos { get; set; } = new List<CustLogInfo>();
+    }
+
+}
